d77831cf69f510544ea04fc1a585ec0414d39e02
mach-12 <mann.compi@gmail.com>
2024-01-15 23:30:47 +0530

    added streamlit integration

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -30,6 +30,11 @@ const Home: NextPage = () => {
             <Faq />
             <Trial />
             <Footer />
+            <iframe
+               src="https://not-by-bot.streamlit.app/embed=true"
+               height="450"
+               style={{ width: "100%", border: "none" }}
+            ></iframe>
          </Box>
       </Layout>
    );
